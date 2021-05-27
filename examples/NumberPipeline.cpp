d516026f44a6d03afd112de72d743847b09d32d5
TSprech <trey.sprecher@gmail.com>
2021-05-27 17:09:35 -0400

    Updating Travis File
    Needs additional build flags to shift to C++17 testing as the lambda autos do not work in the default build environment (as it uses C++11).

--- examples/NumberPipeline.cpp
+++ examples/NumberPipeline.cpp
@@ -2,7 +2,10 @@
 // Seperator because the Arduino library doesn't play nice with the standard library
 #include <Arduino.h>
 
+// This is an input, it takes in no parameters and returns a value
 pipes::PullInput<int> constant_2 = []() { return 2; };
+
+// This is an output, it takes in a parameter (input) and returns nothing
 pipes::PullOutput<int> print_int = [](auto input) { Serial.print(input); };
 
 void setup() {
@@ -11,7 +14,7 @@ void setup() {
     while(!Serial);
   // clang-format on
 
-  print_int << constant_2; // This should print "2"
+  print_int << constant_2;  // This should print "2" because 2 is being returned from constant_2 and passed to print_int which prints it
 }
 
 void loop() {
